3becdfc7e5850ad714f510350e76f881ff93917a
STEFANO LUIZ FERREIRA JUNIOR <stefano.lfjunior@senacsp.edu.br>
2019-08-13 21:31:48 -0300

    estrutura if composta

--- operadores.js
+++ operadores.js
@@ -1,3 +1,23 @@
 let nome = "João";
 let idade = 18;
 const sexo = "M";
+
+if (nome === "Stefano" && idade === 18) {
+    console.log("Entrou no if")
+}
+else {
+    console.log("Entrou no else")
+}
+
+if (idade >= 18 && sexo == "M") {
+    console.log("Voce nao possui desconto");
+}
+else if (idade >= 18 && sexo == "M") {
+    console.log("Voce tem 20% desconto e um refrigerante");
+}
+else if (sexo == "F") {
+    console.log("Voce tem 20% desconto");
+}
+else {
+    console.log("Voce tem 10% desconto");
+}
